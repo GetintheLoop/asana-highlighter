5328ed09780b5ebd664d9b4a6b9fe6b4146b12ac
lukas-gitl <lukas@getintheloop.ca>
2017-09-13 16:32:58 -0700

    Update asana-in-bitbucket.js

--- scripts/asana-in-bitbucket.js
+++ scripts/asana-in-bitbucket.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Asana Links in Bitbucket
 // @namespace    https://github.com/GetintheLoop/asana-highlighter/
-// @version      0.4
+// @version      0.5
 // @description  Use Asana Task Id and render as links in Bitbucket
 // @author       Lukas Siemon
 // @match        https://bitbucket.org/*
@@ -13,17 +13,16 @@
     'use strict';
     setInterval(function() {
         if (!document.hidden) {
-            let desc = $(".pull-request-title h1");
-            if (desc.length === 1) {
-                if (desc[0].innerText.indexOf('#') !== -1) {
-                    desc[0].innerHTML = desc[0].innerText.replace(
+            $(".pull-request-title h1, a.pull-request-title").each(function(index, desc) {
+                if (desc.innerText.indexOf('#') !== -1) {
+                    desc.innerHTML = desc.innerText.replace(
                         /#(\d{10,})/g,
                         '<a href="https://app.asana.com/0/0/$1" target="_blank">' +
                         '<img src="http://i.imgur.com/gsoSXCM.png" style="height:20px; vertical-align:middle">' +
                         '</a>'
                     );
                 }
-            }
+            });
         }
     }, 1000);
 })();
