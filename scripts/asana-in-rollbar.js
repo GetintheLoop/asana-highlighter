d09fbd6f7a8c997aec88047e95575e0ea6ca1c10
simlu <simlu@users.noreply.github.com>
2018-04-02 10:08:42 -0700

    Using last occurrence
    Now using last index of asana link to allow for placing link in comments.

--- scripts/asana-in-rollbar.js
+++ scripts/asana-in-rollbar.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Asana in Rollbar Summary
 // @namespace    https://github.com/GetintheLoop/asana-highlighter/
-// @version      0.6
+// @version      0.7
 // @description  Display Asana Tasks in Rollbar List View
 // @author       Lukas Siemon
 // @match        https://rollbar.com/*
@@ -26,8 +26,9 @@
                             method: "GET",
                             success: function(data){
                                 let splitter = 'href="https://app.asana.com/';
-                                if (data.partial.indexOf(splitter) !== -1) {
-                                    let match = data.partial.split(splitter)[1].split('"')[0];
+                                let lastIndex = data.partial.lastIndexOf(splitter);
+                                if (lastIndex !== -1) {
+                                    let match = data.partial.substring(lastIndex + splitter.length).split('"', 1)[0];
                                     td.html("<a title='Asana Task' target='_blank' style='cursor:pointer' href='https://app.asana.com/" + match + "'>" +
                                             "<img width='24px' height='24px' src='https://i.imgur.com/EPeKqJ5.png'></a>");
                                 } else {
